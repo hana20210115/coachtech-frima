d9805db87679e01d585c732fe799bbe42d821e2f
hana20210115 <hana20210115@gmail.com>
2026-04-27 11:54:33 +0900

    プロフィール編集機能の実装

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ProfileRequest;
+use App\Models\Item;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function edit()
+    {
+        $user = Auth::user();
+        return view('mypage_edit', compact('user'));
+    }
+
+    public function update(Request $request)
+{
+    
+    $user = Auth::user();
+    
+   
+    $data = $request->only(['name', 'postcode', 'address', 'building_name']);
+
+  
+    if ($request->hasFile('image')) {
+        $path = $request->file('image')->store('profiles', 'public');
+        $data['image'] = $path; // $data配列に画像パスを追加
+    }
+
+ 
+    $user->update(['name' => $data['name']]);
+
+
+    $user->profile()->updateOrCreate(
+        ['user_id' => $user->id],
+        [
+            'image' => $data['image'] ?? $user->profile?->image,
+            'postcode' => $data['postcode'],
+            'address' => $data['address'],
+            'building_name' => $data['building_name'],
+        ]
+    );
+
+    return redirect()->route('mypage.index')->with('status', 'プロフィールを更新しました');
+}
+    
+        
+
+      
+    public function index(Request $request)
+    {
+       
+        $user = Auth::user()->load('profile');
+
+       
+
+        
+        $sellItems = Item::where('user_id', $user->id)->get();
+
+        $buyItems = Order::where('user_id', $user->id)
+        ->with('item')
+        ->get()
+        ->pluck('item');
+
+        return view('mypage', compact('user', 'sellItems', 'buyItems'));
+}
+}

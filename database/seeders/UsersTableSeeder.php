b1d4f63502533739d2def35bc4c0e889ddbfca11
hana20210115 <hana20210115@gmail.com>
2026-04-30 13:41:03 +0900

    READMEテスト

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Profile;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'test_user',
+            'email' => 'test@example.com',
+            'password' => \Hash::make('password'),
+        ]);
+
+    Profile::create([
+            'user_id' => $user->id,
+            'postcode' => '123-4567',
+            'address' => '東京都',
+            'building' => 'オフィスビル',
+            'image' => null, 
+        ]);
+    }
+    }
